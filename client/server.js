4471fe3c35f04010860c61b7c48635bd0a5225f8
paranerd <paranerd.development@gmail.com>
2020-12-31 16:03:09 +0100

    Added server.js to client

---
+++ client/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+
+router.get('*', function(req, res) {
+    res.sendFile('index.html', { root: __dirname + '/dist/openphotos' });
+});
+
+app.use(express.static(__dirname + '/dist/openphotos'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use('/', router);
+
+// Start server
+app.listen(port, () => console.log(`Listening on port ${port}`));
