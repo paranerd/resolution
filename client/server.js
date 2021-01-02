9033e012dae8640c73dd24d27ce5000539847264
paranerd <paranerd.development@gmail.com>
2021-01-02 22:12:23 +0100

    Use path.join instead of simple concatenation

--- client/server.js
+++ client/server.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 const router = express.Router();
+
+// Read .env
 require('dotenv').config();
 
+// Set port
 const port = process.env.PORT || 8080;
 
-router.get('*', function(req, res) {
-    res.sendFile('index.html', { root: __dirname + '/dist/openphotos' });
+// Route all requests to Angular's index.html
+router.get('*', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'dist', 'openphotos') });
 });
 
+// Handle requests to static assets
 app.use(express.static(__dirname + '/dist/openphotos'));
 
+// Set up body parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+// Connect router to app
 app.use('/', router);
 
 // Start server
