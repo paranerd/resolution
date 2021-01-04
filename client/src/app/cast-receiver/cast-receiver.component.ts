89f51a9c93c7d889293b87847697d9b3c4413e36
paranerd <paranerd.development@gmail.com>
2021-01-04 12:42:23 +0100

    Cleaned up cast-receiver component

--- client/src/app/cast-receiver/cast-receiver.component.ts
+++ client/src/app/cast-receiver/cast-receiver.component.ts
@@ -13,21 +13,13 @@ export class CastReceiverComponent implements OnInit {
     castSdkLoaded: boolean = false;
     customJsLoaded: boolean = false;
 
-    constructor() {
-        // Load cast resources
-        /*if (!this.castSdkLoaded) {
-            // Load framework
-            this.loadScript('//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js');
-
-            // Load logger
-            this.loadScript('//www.gstatic.com/cast/sdk/libs/devtools/debug_layer/caf_receiver_logger.js');
-            this.castSdkLoaded = true;
-        }*/
-  }
+    constructor() {}
 
     ngOnInit(): void {}
 
     ngAfterViewInit(): void {
+        // Load receiver JS
+        // Note the imports in index.html which are required in <head>
         if (!this.customJsLoaded) {
             this.loadScript('/assets/js/receiver.js');
         }
@@ -43,8 +35,6 @@ export class CastReceiverComponent implements OnInit {
         const script = document.createElement('script');
         script.innerHTML = '';
         script.src = url;
-        //script.async = true;
-        //script.defer = true;
         body.appendChild(script);
     }
 }
