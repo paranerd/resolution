b1f6a6b908ae6b9709e6a59ea1dbb3a7284c8922
paranerd <paranerd.development@gmail.com>
2021-11-29 14:29:55 +0100

    Added Jest testing

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jest'],
   parserOptions: {
     ecmaVersion: 13,
   },
