f921df0003dd4df2d9cdb54dd12f813d34cd271d
paranerd <paranerd.development@gmail.com>
2021-12-02 12:37:32 +0100

    Fixed database.js does not exist in Docker build by moving database.js to util/

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ const app = require('./app');
 // Read ENV variables
 require('dotenv').config();
 
+// Read local ENV variables
+require('dotenv').config({ path: '.env.local' });
+
 // Connect to MongoDB
-require('./config/database').connect();
+require('./util/database').connect();
 
 const port = process.env.PORT || 8080;
 
