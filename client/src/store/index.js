22c72245372e9461ecbff5b0b78f1b883f0b7a73
paranerd <paranerd.development@gmail.com>
2021-12-03 22:28:35 +0100

    Added store functions to add and remove items

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -9,12 +9,48 @@ const store = createStore({
     };
   },
   mutations: {
+    /**
+     * Set items.
+     *
+     * @param {object} state
+     * @param {Array} items
+     */
     setItems(state, items) {
       state.items = items;
     },
+    /**
+     * Add items to existing set.
+     *
+     * @param {object} state
+     * @param {Array} items
+     */
+    addItems(state, items) {
+      state.items = [...state.items, ...items];
+    },
+    /**
+     * Remove items.
+     *
+     * @param {object} state
+     * @param {Array<String>} ids
+     */
+    removeItems(state, ids) {
+      state.items = state.items.filter((item) => !ids.includes(item.id));
+    },
+    /**
+     * Set item as selected.
+     *
+     * @param {object} state
+     * @param {String} itemId
+     */
     select(state, itemId) {
       state.selected.push(itemId);
     },
+    /**
+     * Set item as unselected.
+     *
+     * @param {object} state
+     * @param {String} itemId
+     */
     unselect(state, itemId = null) {
       if (itemId) {
         state.selected = state.selected.filter((id) => id !== itemId);
@@ -22,6 +58,12 @@ const store = createStore({
         state.selected = [];
       }
     },
+    /**
+     * Set Cast App ID.
+     *
+     * @param {object} state
+     * @param {String} id
+     */
     setCastAppId(state, id) {
       state.castAppId = id;
     },
