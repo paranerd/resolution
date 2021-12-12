196b65e8fa8116d9eea7fb5cdb9d44d870167d16
paranerd <paranerd.development@gmail.com>
2021-12-12 21:39:55 +0100

    ESLint config files

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,19 +6,19 @@ module.exports = {
       },
     },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
       .rule('vue')
       .use('vue-loader')
-        .tap(options => {
-          options.compilerOptions = {
-            ...options.compilerOptions,
-            isCustomElement: tag => tag.startsWith('google-cast-launcher')
-          }
-          return options
-        })
+      .tap((options) => {
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: (tag) => tag.startsWith('google-cast-launcher'),
+        };
+        return options;
+      });
   },
   devServer: {
-    port: 3000
-  }
-}
+    port: 3000,
+  },
+};
